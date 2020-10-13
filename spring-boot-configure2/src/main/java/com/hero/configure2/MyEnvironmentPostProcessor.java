dde7e536421340a7ea89df3a3146b763431ac11e
hero-joey <zhaoyaxiong@aliyun.com>
2020-10-13 23:55:17 +0800

    配置文件读取

--- spring-boot-configure2/src/main/java/com/hero/configure2/MyEnvironmentPostProcessor.java
+++ spring-boot-configure2/src/main/java/com/hero/configure2/MyEnvironmentPostProcessor.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.PropertiesPropertySource;
-import org.springframework.core.env.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.io.FileInputStream;
@@ -23,13 +22,14 @@ public class MyEnvironmentPostProcessor implements EnvironmentPostProcessor {
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
 
+        //配置文件集中化管理，也可以参照该种方式实现
         try (InputStream inputStream = new FileInputStream("F://user.properties")) {
             Properties properties = new Properties();
             properties.load(inputStream);
             PropertiesPropertySource propertiesPropertySource = new PropertiesPropertySource("MyProperties", properties);
             environment.getPropertySources().addLast(propertiesPropertySource);
 
-        } catch(IOException ex) {
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
 
