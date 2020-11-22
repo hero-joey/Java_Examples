3000ba0f2df017e55e5a9bd44837b062a3690155
hero-joey <zhaoyaxiong@aliyun.com>
2020-11-22 23:28:48 +0800

    contextInitializer初始化

---
+++ javaee/proxy/jdkProxyInstance/src/com/hero/proxy/GPProxy.java
@@ -0,0 +1,12 @@
+package com.hero.proxy;
+
+/**
+ * @description: GPProxy
+ * @date: 2020/11/21
+ * @author: bear
+ * @version: 1.0
+ */
+public class GPProxy {
+    public static final String ln = "\r\n";
+    public static Object newProxyInstance(G)
+}
