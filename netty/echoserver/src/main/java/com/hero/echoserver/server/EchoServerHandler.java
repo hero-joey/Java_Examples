9673c145d724839bf223107b412c2a0930b9524e
hero-joey <zhaoyaxiong@aliyun.com>
2021-01-04 23:13:30 +0800

    netty echoserver

---
+++ netty/echoserver/src/main/java/com/hero/echoserver/server/EchoServerHandler.java
@@ -0,0 +1,35 @@
+package com.hero.echoserver.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @description: EchoServerHandler
+ * @date: 2021/1/4
+ * @author: bear
+ * @version: 1.0
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf byteBuf = (ByteBuf)msg;
+        System.out.println("Server received: " + byteBuf.toString(CharsetUtil.UTF_8));
+        ctx.write(byteBuf);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+                .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ netty/echoserver/src/test/java/com/hero/echoserver/EchoAppTest.java
@@ -0,0 +1,20 @@
+package com.hero.echoserver;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class EchoAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
