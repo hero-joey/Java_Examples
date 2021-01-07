83fa4260092eaa74416e54eecdf98e619df1ab7b
hero <zhaoyaxiong@aliyun.com>
2021-01-07 13:34:54 +0800

    netty btebuf使用

--- netty/test-server/src/test/java/com/hero/demo/ByteBufTypeTest.java
+++ netty/test-server/src/test/java/com/hero/demo/ByteBufTypeTest.java
@@ -2,6 +2,8 @@ package com.hero.demo;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.CompositeByteBuf;
+import io.netty.buffer.Unpooled;
 import org.junit.Test;
 
 /**
@@ -42,5 +44,26 @@ public class ByteBufTypeTest {
             System.out.println(new String(array));
             System.out.println("direct bytebuf");
         }
+    }
+
+    private void sendMsg(CompositeByteBuf compositeByteBuf) {
+        //处理整个消息
+        for (ByteBuf byteBuf : compositeByteBuf) {
+            int length = byteBuf.readableBytes();
+            byte[] array = new byte[length];
+            //将CompositeByteBuf中的数据复制到数组中
+            byteBuf.getBytes(byteBuf.readerIndex(), array);
+            //处理一下数组中的数据
+            System.out.print(new String(array));
+        }
+        System.out.println();
+    }
+
+    public void testCompositeByteBuf() {
+        CompositeByteBuf compositeByteBuf = ByteBufAllocator.DEFAULT.compositeBuffer();
+        ByteBuf headBuf = Unpooled.copiedBuffer("疯狂创客圈".getBytes());
+        ByteBuf bodyBuf = Unpooled.copiedBuffer("Netty实战".getBytes());
+
+        compositeByteBuf.addComponents(headBuf, bodyBuf);
     }
 }
